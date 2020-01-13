d7f71799db509344987b458b9466e500b5cb6562
Rachael Yoder <wayoder4@gmail.com>
2020-01-13 16:53:43 -0500

    initial commit

---
+++ src/components/JumbotronLanding.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Jumbotron } from "reactstrap";
+
+export default class JumbotronLanding extends React.Component {
+    render() {
+        return (
+            <div>
+                <Jumbotron>
+                    <h1>
+                        Rachael Yoder
+                    </h1>
+                    <hr />
+                </Jumbotron>
+            </div>
+        );
+    }
+}

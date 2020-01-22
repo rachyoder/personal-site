1e284a9c48acdc4aa1c18fc83960f126f1ef2518
Rachael Yoder <wayoder4@gmail.com>
2020-01-22 13:37:20 -0500

    setting up main styling

--- src/components/JumbotronLanding/JumbotronLanding.js
+++ src/components/JumbotronLanding/JumbotronLanding.js
@@ -4,10 +4,23 @@ export default class JumbotronLanding extends React.Component {
     render() {
         return (
             <div className="jumbotron main my-auto">
-                <h1 className="display-3" >
+                <h1 className="display-1 text-center" >
                     Rachael Yoder
                     </h1>
                 <hr />
+                <nav>
+                    <ul className="nav">
+                        <li>
+                            Projects
+                        </li>
+                        <li>
+                            Blog Posts
+                        </li>
+                        <li>
+                            Contact
+                        </li>
+                    </ul>
+                </nav>
             </div>
         );
     }
